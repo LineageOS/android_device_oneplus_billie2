04bd82aad1de5041cd2b73898451eb0fa01283fa
Nick Reuter <nreuter85@gmail.com>
2024-12-10 20:32:13 -0600

    billie2: Address missing libbinder symbols in libwfdnative
    _ZN7android6binder12atrace_beginEmPKc
    _ZN7android6binder10atrace_endEm
    
    Change-Id: I942d51721461dbbf9c8d0f2f32128529bb7480c3

--- extract-files.py
+++ extract-files.py
@@ -61,6 +61,7 @@ blob_fixups: blob_fixups_user_type = {
         .replace_needed('libdisplayconfig.qti.so', 'libdisplayconfig.system.qti.so'),
     'system_ext/lib64/libwfdnative.so': blob_fixup()
         .replace_needed('android.hidl.base@1.0.so', 'libhidlbase.so')
+        .add_needed('libbinder_shim.so')
         .add_needed('libinput_shim.so'),
     'system_ext/lib64/lib-imsvideocodec.so': blob_fixup()
         .add_needed('libgui_shim.so'),
