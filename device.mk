56dff051355a3516e6439b44f20a2fc19051806a
LuK1337 <priv.luk@gmail.com>
2025-01-18 18:33:32 +0100

    billie2: Move WfdCommon boot jar declaration to vendor
    Change-Id: I8fa2c0f64692873b432cc252ec88b388a32023a1

--- device.mk
+++ device.mk
@@ -423,10 +423,6 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/wifi/p2p_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/p2p_supplicant_overlay.conf \
     $(LOCAL_PATH)/wifi/wpa_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant_overlay.conf
 
-# WiFi Display
-PRODUCT_BOOT_JARS += \
-    WfdCommon
-
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
 # WiFi firmware symlinks
