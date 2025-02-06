3ac47fdc450c519f434e79d9e827268cdc65f02b
Bruno Martins <bgcngm@gmail.com>
2025-02-06 21:32:31 +0000

    billie2: Use aidl sensors multihal
    Change-Id: I21da3da39e9726dca693d6bcbe42e93be861f7c7

--- device.mk
+++ device.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2018-2022 The LineageOS Project
+# Copyright (C) 2018-2025 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -335,7 +335,7 @@ PRODUCT_PACKAGES += \
 
 # Sensors
 PRODUCT_PACKAGES += \
-    android.hardware.sensors@2.0-service.multihal
+    android.hardware.sensors-service.multihal
 
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
