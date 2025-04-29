c56a7e1c0914f19940d5eea9f7959d435d3a80a6
Bruno Martins <bgcngm@gmail.com>
2025-04-29 23:37:50 +0100

    billie2: Migrate Lineage Health to soong_config_set
    Change-Id: I2eff39e83b9673aa2989d05de65c646241ba61da

--- device.mk
+++ device.mk
@@ -275,6 +275,8 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     vendor.lineage.health-service.default
 
+$(call soong_config_set,lineage_health,charging_control_charging_path,/sys/class/power_supply/battery/mmi_charging_enable)
+
 # LiveDisplay
 PRODUCT_PACKAGES += \
     vendor.lineage.livedisplay@2.1-service.oneplus
